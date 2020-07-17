5649c036bf86af62333ff7b20ca1478581b6c9d2
haileyhansard <haileyhansard@gmail.com>
2020-07-17 15:30:16 -0700

    Updated App, Smurf, SmurfList, now getting and posting smurfs to server

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,37 +1,61 @@
-import React, { Component, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { SmurfContext }  from '../contexts/SmurfContext';
 import SmurfsList from './SmurfsList';
-import axios from 'axios';
+import SmurfsForm from './SmurfsForm';
 
-//import SmurfsForm from './SmurfsForm';
+import "./App.css";
+import axios from "axios";
 
-import { SmurfContext } from '../contexts/SmurfContext';
 
-class App extends Component {
+const App = () => {
+  const [newSmurf, setNewSmurf] = useState({
+    name: '',
+    age: '',
+    height: ''
+  })
+  const [smurfs, setSmurfs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        setSmurfs(res.data)
+      })
+      .catch(err => {
+        console.log("error", err)
+      })
+    }, []);
   
-  // const [newSmurf, setNewSmurf] = useState([smurfs]);
+    const onChange = e => {
+      const smurf = e.target.name
+      setNewSmurf({
+        ...newSmurf,
+        [smurf]: e.target.value
+      })
+    }
 
+    const onSubmit = (e) => {
+      e.preventDefault();
+      axios
+        .post("http://localhost:3333/smurfs", {...newSmurf})
+        .then((res => {
+          setSmurfs(res.data)
+        }))
+      };
 
-  state = {
-    list: []
-  };
-
-
-
-  render() {
-    return (
+  return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        <SmurfContext.Provider>
-          {/* <SmurfsForm /> */}
+        <SmurfContext.Provider value={{ onChange, onSubmit, smurfs }}>
+          <SmurfsForm />
           <SmurfsList />
         </SmurfContext.Provider>
       </div>
     );
-  }
 }
+
 
 export default App;
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,42 +1,19 @@
-
 import React, { useContext } from 'react';
-import axios from 'axios';
-import {SmurfContext} from '../contexts/SmurfContext';
 import Smurf from './Smurf';
-import { render } from 'react-dom';
+import {SmurfContext} from '../contexts/SmurfContext';
 
+const SmurfsList = () => {
+  const {smurfs} = useContext(SmurfContext);
 
-const SmurfsList = props => {
-    state= {
-        smurfs: [],
-    };
-
-    const {smurfList} = useContext(SmurfContext);
-
-    componentDidMount(); {
-        axios
-          .get("http://localhost:3333/smurfs")
-          .then( res => {
-            console.log("Response", res.data)
-            this.setState({
-              ...this.state, smurfs: res.data
-            })
-          }) 
-          .catch(err => {
-            console.log("Something went wrong", err)
-          });
-        };
-
-  
-     return(
-        <div>
-            <h1>Smurfs List</h1>
-            {SmurfsList.map(smurf => (
-                <Smurf key={smurf.id} smurf={smurf} />
-            ))}
-        </div>
-    )      
-
- 
+  return (
+    <div>
+      <h2>List of All Smurfs</h2>
+      {smurfs.map(smurf => (
+        <Smurf key={smurf.id} smurf= {smurf} />
+      )
+        )}
+    </div>
+  )
 };
+
 export default SmurfsList;
